3938763fb0f17df0fda9532ea5880654bb7b6e97
tcano0505 <tommy.cano19@my.stjohns.edu>
2023-07-18 01:09:38 -0400

    demo

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a082264e17mshee22b952b5e9aa2p1529e1jsn4a3feae3c47a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const geo_API_url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3c855a46ffe712fc8b7802d231c0f78b";
